c0035612a514e16625b290a7771bc1a87bc6174a
Dohertyl1127 <111251142+dohertyl1127@users.noreply.github.com>
2023-02-22 11:54:50 +1300

    Add files via upload

---
+++ 01_Lucky_Unicorn/00_Sandbox.py
@@ -0,0 +1,6 @@
+#ask for number from user
+user_answer = int(input("choose a number to be multiplied by 5"))
+#multiply input by 5
+answer = user_answer * 5
+#print output
+print("{} multiplied by 5 is {}".format(user_answer, answer))
