a7fed89d1c22a2309a50de5ed85c89e293abbdcd
Dohertyl1127 <111251142+dohertyl1127@users.noreply.github.com>
2023-02-24 09:59:01 +1300

    Add files via upload

---
+++ 01_Lucky_Unicorn/01_Lucky_Unicorn/00_Sandbox.py
@@ -0,0 +1,8 @@
+#count from one up to ten
+
+name = ""
+while name.lower() != "xxx":
+    name = input("who are you? ")
+    print(name)
+print()
+print("we are done!")
---
+++ 01_Lucky_Unicorn/01_Lucky_Unicorn/01_Yes_No_v1.py
@@ -0,0 +1,9 @@
+#ask user if they have played before
+show_instuctions = input("have you played before? ")
+
+#if yes continue code
+if show_instuctions.lower() == "yes":
+    print("program continues")
+#if no display instrutions
+else:
+    print("display instructions")
