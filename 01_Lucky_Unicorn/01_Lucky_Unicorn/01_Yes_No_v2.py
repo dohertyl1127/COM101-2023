de3849b5ae820ac5bd3f181eb4f96bf70004822e
Dohertyl1127 <111251142+dohertyl1127@users.noreply.github.com>
2023-02-28 12:47:44 +1300

    Add files via upload

--- 01_Lucky_Unicorn/01_Lucky_Unicorn/01_Yes_No_v2.py
+++ 01_Lucky_Unicorn/01_Lucky_Unicorn/01_Yes_No_v2.py
@@ -6,14 +6,10 @@ while show_instructions.lower != "xxx":
     show_instructions = input("have you played before? ")
 
     #if yes continue code
-    if show_instructions.lower() == "yes":
-        print("program continues")
-    elif show_instructions.lower() == "y":
+    if show_instructions.lower() == "yes" or "y":
         print("program continues")
     #if no display instrutions
-    elif show_instructions.lower() == "no":
-        print("display instructions")
-    elif show_instructions.lower() == "n":
+    elif show_instructions.lower() == "no" or "n":
         print("display instructions")
     else:
          print("please choose yes/no")
